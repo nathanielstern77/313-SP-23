95ba36a32d2049fc9d34cf77f34bb194961fd556
nathanielstern77 <85973845+nathanielstern77@users.noreply.github.com>
2023-03-08 17:37:30 -0600

    added all templats

---
+++ Mid-Terms/Brynn/sketch.js
@@ -0,0 +1,43 @@
+let mysound
+let mysound2
+
+function preload(){
+  mysound=loadSound('./assets/1.mp3')
+  mysound2=loadSound('./assets/2.mp3')
+}
+
+function setup() {
+  // put setup code here
+  createCanvas(500, 500);
+  background('white')
+}
+
+function draw() {
+  // put drawing code here
+}
+
+function mousePressed() {
+
+  if (mouseX<width/2){
+
+  if (mysound.isPlaying()) {
+    // .isPlaying() returns a boolean
+    mysound.stop();
+    background(255, 0, 0);
+  } else {
+    mysound.play();
+    background(0, 255, 0);
+  }}
+
+else {
+  if (mysound2.isPlaying()) {
+    // .isPlaying() returns a boolean
+    mysound2.stop();
+    background(255, 0, 0);
+  } else {
+    mysound2.play();
+    background(0, 255, 0);
+  }
+}
+
+}
