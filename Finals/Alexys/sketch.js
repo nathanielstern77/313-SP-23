93e89505f7dda5ed6cdea7682b4adf3135315174
lexusalexys <alexyshaberkorn@gmail.com>
2023-04-27 10:58:16 -0500

    got it working

--- Finals/Alexys/sketch.js
+++ Finals/Alexys/sketch.js
@@ -1,6 +1,6 @@
-//let img, pixel, pixel2, x=0,y=0
+let img, pixel, pixel2, x=0,y=0
 function preload() {
-  //img=loadImage('screamingfrog.jpg')
+  img=loadImage('assets/screamingfrog.jpg')
 }
 
 function setup() {
@@ -12,12 +12,18 @@ function setup() {
 }
 
 function draw() {
-  fill(255,200,7)
-  background(220,5);
+  pixel=get(x,y,x+5,y+5)
+  pixel2=get(x+1,100)
+set(x+5,y,pixel)
 
-  x+=102
-  if(x>width){x=0
-  y+=10}
+//console.log(r,g,b)
+  //set(x+1,100,r,g,b)
+  //updatePixels()
+  //console.log(pixel[1])
+  //console.log(pixel2)
+  //x+=102
+  //if(x>width){x=0
+  //y+=10}
   
   text('ribbet', random(width-5), random(height-5))
   if(y>height){y=10}
