4adc56cc7b29c9ecceae089316a5f25294008447
lexusalexys <alexyshaberkorn@gmail.com>
2023-05-04 12:02:40 -0500

    bugs

--- Finals/Alexys/sketch.js
+++ Finals/Alexys/sketch.js
@@ -3,20 +3,31 @@ let text2=['potato','frank','go']
 
 let img, pixel, pixel2, x=0,y=0
 
-function preload(){ 
- img=loadImage('assets/2.jpg')
+let photo = []
+let numPhoto = 4
 
+function preload(){ 
+ //img=loadImage('assets/2.jpg')
+ let photo1 = loadImage('assets/1.jpg');
+ let photo2 = loadImage('assets/2.jpg');
+ let photo3 = loadImage('assets/3.png');
+ let photo4 = loadImage('assets/4.png');
+ photo = [photo1,photo2,photo3,photo4];
+ 
  song = loadSound('assets/peopletalking.mp3')
  //song = loadSound('assets/song2.mp3');
+
 }
 function setup() {
   createCanvas(windowWidth, windowHeight);
- 
+
   frameRate(5) //text rate
+
+  noLoop();
   
-  image(img,0,0,width,height);
-  tint(255, 50);
-  image(img, 50, 0);
+  ///image(img,0,0,width,height);
+
+  //image(img, 50, 0);
   
   strokeWeight(8) //for mouse tracker width
   index = round(random(0, 4))
@@ -24,17 +35,25 @@ function setup() {
 }
 
 function draw() {
-textSize(random(20, 70))
+  background(255);
+
+  imageMode(CENTER);
+  let randoImage = random(photo);
+  image(randoImage, width/2, height/2); 
+  
+  textSize(random(20, 70))
   textFont("Times New Roman")
   fill( random(0,200), random(0,200), random(0,200) )
-  let theWord = randoWord(mytext) //getting a random word from the array I am sending
-  let word2 = randoWord(text2)
+  let theWord = randoWord(mytext)
   let sentence = (theWord)
-  let x = random(width-100) // generate a random x coordinate
-  let y = random(height-30) // generate a random y coordinate
+  let x = random(width-100)
+  let y = random(height-30)
   text(sentence, x, y)
+  
   line(mouseX, mouseY, pmouseX, pmouseY);
 }
+
+
 function mousepressed() {
   x += 1;
   refresh();
